5e1da6af40327eab91e02d3529198749521e8f82
humayonitbd <humayonforid44@gmail.com>
2025-07-07 10:18:32 +0600

    update url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
-    return "http://10.0.70.163:5001/api/v1";
+    return "http://31.97.76.173:5001/api/v1";
     // return "http://localhost:5000/api/v1";
   };
   
